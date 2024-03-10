34b0fcf88a8bd36e6ffcd06e39445d0138e2672c
Greg <74878050+GPlays09@users.noreply.github.com>
2024-03-10 20:47:54 +0100

    Fixed CreateFile file not found

--- verify.bat
+++ verify.bat
@@ -1,4 +1,4 @@
 @echo off
-
+mkdir build
 tools-builder\arduino-builder -compile -logger=machine -hardware hardware -tools tools-builder -tools hardware\tools -libraries libraries -fqbn=arduino:avr:leonardo -ide-version=10819 -build-path build -warnings=all -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.arduinoOTA.path=hardware\tools -prefs=runtime.tools.arduinoOTA-1.3.0.path=hardware\tools -prefs=runtime.tools.avrdude.path=hardware\tools -prefs=runtime.tools.avrdude-6.3.0-arduino17.path=hardware\tools -prefs=runtime.tools.avr-gcc.path=hardware\tools -prefs=runtime.tools.avr-gcc-7.3.0-atmel3.6.1-arduino7.path=hardware\tools -verbose user_script.cpp
 pause
