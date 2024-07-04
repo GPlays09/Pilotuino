8cb422cdde148f23cbb48b5f9c06901058b6b99f
Greg <74878050+GPlays09@users.noreply.github.com>
2024-07-04 19:50:05 +0200

    some comments added

--- user_script.cpp
+++ user_script.cpp
@@ -1,24 +1,20 @@
+// test includes (not necessary)
 #include <Mouse.h>
 #include <Keyboard.h>
 
 const DeviceDescriptor dDscr = D_DEVICE(0x00, 0x00, 0x00, 64, 0x1532 /*vid*/, 0x0043 /*pid*/, 0x100, IMANUFACTURER, IPRODUCT, ISERIAL, 1);
-const uint8_t* dStr[3] = {"asd", "fgh", "jkl"};
+const uint8_t* dStr[3] = {"Manufacturer", "Product", "SerialNumber"}; // device descriptor strings
 
-int main(void)
-{
+int main(void) {
 	init();  // wiring.c
 	USBDevice.attach(/*true, &dDscr, dStr*/);
-
-	for (;;) 
-	{
+	// comport enabled, device descriptor ptr, const char array of arrays ptr
+	// default: true, vid: 0x2341 pid: 0x8036, "Arduino_LLC" "USB_IO_Board" "..."
+	
+	// adding or initializing of user-defined descriptors must happen after .attach();
+	
+	for (;;) {
 		if (serialEventRun) {serialEventRun();}
-		
-		/*
-		Mouse.move(2,0,0);
-		delay(1000);		
-		Mouse.move(-2,0,0);
-		delay(1000);
-		*/
 	}
 
 	return 0;
